728417cc801ac54e31a195ed85623564643fbce8
Matvei98 <Matthias78@yandex.ru>
2023-03-01 17:10:34 +0300

    added styles to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,6 +12,7 @@ footertitle?:string
 export const Footer:React.FC<Props>= ({footertitle}) =>{
 return(
 <> 
+<div className='footer'>
 <div className='Aboutweb'>
 <img src={Logor} alt="kokok"/> 
 <p  className='LinkLittle'>Digitaldastin by Dastin Darmawan</p>
@@ -24,7 +25,7 @@ return(
 </div>
 <div className='Page'>
 <div className='Links'>
-<a href='www' className='LinkHeadl'>Category</a>
+<a href='www' className='Linkheadls'>Category</a>
 <a href='www' className='LinkLittle'>CSS</a>
 <a href='www' className='LinkLittle'>Javascript</a>
 <a href='www' className='LinkLittle'>Tailwind</a>
@@ -32,18 +33,18 @@ return(
 <a href='www' className='LinkLittle'>More Category</a>
 </div>
 <div className='Links'>
-<a href='www' className='LinkHeadl'>About Me</a>
+<a href='www' className='Linkheadls'>About Me</a>
 <a href='www' className='LinkLittle'>About Me</a>
 <a href='www' className='LinkLittle'>Projects</a>
 <a href='www' className='LinkLittle'>Achievement</a>
 </div>
 <div className='Links'>
-<a href='www' className='LinkHeadl'>Get in touch</a>
+<a href='www' className='Linkheadls'>Get in touch</a>
 <a href='www' className='LinkLittle'>+62-8XXX-XXX-XX</a>
 <a href='www' className='LinkLittle'>demo@gmail.com</a>
 </div>
 <div className='Links'>
-<a href='www' className='LinkHeadl'>Follow Us</a>
+<a href='www' className='Linkheadls'>Follow Us</a>
 <a href='www' className='LinkLittle'>Medium</a>
 <a href='www' className='LinkLittle'>Instagram</a>
 <a href='www' className='LinkLittle'>Twitter</a>
@@ -55,6 +56,7 @@ return(
 <div className='text4'>
 <p className='textlittle'>© 2022 Digitaldastin</p>
 <p className='textlittle'>Made With ❤️ Jakarta, Indonesia</p>
+</div>
 </div>
 </div>
 </>
